e316cfe77589ac0187c44e21a0d3d15410dc75c5
Martin Moya <moyamartin1@gmail.com>
2020-11-15 01:14:50 -0300

    Fits the kalman filter for the model
    * Adds matrix ocv vs soc generated from Lucio's OCV_vs_SOC oversampled
    curve and Fede's SOC 55 samples
    * Generates a System State Model discretized for 0.1 Ts stored in
    battery_state_space_model_0.1.mat. This dataset contains the OCV_vs_SOC,
    discretized matrices and Electrical model matrices

--- Kalman/kalman_filter_soc.m
+++ Kalman/kalman_filter_soc.m
@@ -2,21 +2,10 @@
 %% Load measurements, matrices and define the kalman filter structure %%
 
 % Load A, B, C, D matrices
-load('Juego_de_Matrices_A_B_C_D_ver_0.1.mat')
-load('Juego_de_parametros_R_C_corregidos')
-load('OCV_VS_SOC_01C.mat')
-SOC_01C = SOC_01C - min(SOC_01C);
-SOC_01C = SOC_01C/max(SOC_01C);
-C = zeros(1, 3);
-
-% Discretize the matrices
-Ts = 0.1;
-A = eye(3) + A*Ts;
-% this value must be fixed and constant across all the SOC
-B(3, 1, :) = 1/(2.9*3600);
-B = B*Ts;
-% Load a drive cycle to test the kalman filter %%
-load('../dataset_18650pf/0degC/Drive Cycles/06-01-17_22.03 0degC_Cycle_4_Pan18650PF.mat')
+load('battery_state_space_model_0.1.mat')
+% Load dataset to test the model
+load(['../dataset_18650pf/25degC/Drive Cycles/' ...
+     '03-19-17_14.31 25degC_Cycle_4_Pan18650PF.mat'])
 
 %% Define the Kalman Filter and run the simulation %%
 % define the battery Kalman Filter
@@ -35,20 +24,17 @@ load('../dataset_18650pf/0degC/Drive Cycles/06-01-17_22.03 0degC_Cycle_4_Pan1865
 % NOTE 2: Pay attention to matrix sizes and order!!
 current_soc = 1;
 SOC_index = find_closest_value(current_soc, SOC_lutable);
-idx = find_closest_value(current_soc, SOC_01C);
-% Here we fix the offset from OCV_01C
-OCV_01C = OCV_01C - 0.059;
-C(:, :) = [-1, -1, OCV_01C(idx)/SOC_01C(idx)];
 soc_kalman_filter = struct( 'x_act', [0.0; 0.0; 1], 'F', A(:, :, SOC_index), ...
                             'G', B(:, :, SOC_index), ...
                             'P_act', [1.0, 0.0, 0.0;
                                       0.0, 1.0, 0.0; 
                                       0.0, 0.0, 1.0], ...
                             'D', D(:, SOC_index), 'R', 0.2, ...
-                            'H', C(:, :), 'Q', zeros(3, 3), 'u', [0.0]);%[0.035, 0, 0;
-                                               % 0, 0.035, 0;
-                                               % 0, 0, 0], ...
-                            
+                            'H', C(SOC_index, :), ...
+                            'Q', [.045, 0,   0;
+                                  0,   .005, 0;
+                                  0,   0,   1.5e-9], ...
+                            'u', 0.0);
 %% Resample step %%                        
 % Guarda el voltage entre rangos 
 measured_voltage = meas.Voltage;
@@ -80,8 +66,6 @@ soc_error = zeros(length(timeline), 1);
 
 % For the extended version update the matrix F and G respectively
 for i = 1:length(timeline)
-    %current_state(i, :) = soc_kalman_filter.x_act;
-
     soc_array(i, 1) = soc_kalman_filter.x_act(3);
     soc_kalman_filter.u = current_resampled(i);
     soc_kalman_filter = kalman_filter_step(soc_kalman_filter, ...
@@ -89,8 +73,7 @@ for i = 1:length(timeline)
     SOC_index = find_closest_value(soc_kalman_filter.x_act(3), SOC_lutable);
     soc_kalman_filter.F = A(:, :, SOC_index);
     soc_kalman_filter.G = B(:, :, SOC_index);
-    idx = find_closest_value(soc_kalman_filter.x_act(3), SOC_01C);
-    soc_kalman_filter.H(:, 3) = OCV_01C(idx)/SOC_01C(idx);
+    soc_kalman_filter.H = C(SOC_index, :);
     soc_kalman_filter.D = D(:, SOC_index);
     soc_error(i) = (soc_kalman_filter.x_act(3) - ah_resample(i))*100;
 end
